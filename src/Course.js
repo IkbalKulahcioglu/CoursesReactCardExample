6d48d7e641185fe6d1cb865242a36d4575cd064f
Ikbal Kulahcioglu <ikbalkulahcioglu@gmail.com>
2023-10-24 17:06:53 +0300

    Get random course button added and project changed

--- src/Course.js
+++ src/Course.js
@@ -1,13 +1,15 @@
-function Course({id,content,title,price,removeOneCourse}) {
-   
-    return <div className="card">
-        <div className="cardTitlePrice">
-            <h2 className="cardTitle">{title}</h2>
-            <h4 className="cardPrice">{price}$</h4>
-        </div>
-        <p>{content}</p>
-        <button className="cardDeleteBtn" onClick={()=>removeOneCourse(id)}>Delete</button>
-    </div>;
+function Course({ id, content, title, price, removeOneCourse }) {
+  return;
+  <div className="card">
+    <div className="cardTitlePrice">
+      <h2 className="cardTitle">{title}</h2>
+      <h4 className="cardPrice">{price}$</h4>
+    </div>
+    <p>{content}</p>
+    <button className="cardDeleteBtn" onClick={() => removeOneCourse(id)}>
+      Delete
+    </button>
+  </div>;
 }
 
 export default Course;
--- src/Courses.js
+++ src/Courses.js
@@ -1,22 +1,68 @@
 import Course from "./Course";
-function Courses({courses,removeCourse}) {
-    console.log(courses)
-    return( 
-    <div className='courseMainDiv'> 
-        <div>
-            <h2>My Courses</h2>
+import { useState } from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+function Courses({ courses, removeCourse }) {
+  const [index, setIndex] = useState(0);
+  const { content, title, price } = courses[index];
+
+  const checkIndex = (index) => {
+    if (index < 0) {
+      return courses.length - 1;
+    }
+    if (index > courses.length - 1) {
+      return 0;
+    }
+    return index;
+  };
+  const prevCourse = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkIndex(newIndex);
+    });
+  };
+  const nextCourse = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkIndex(newIndex);
+    });
+  };
+  const getRandomCourse = () => {
+    let randomNumber = Math.floor(Math.random() * courses.length);
+    if(randomNumber === index ){
+        randomNumber = index + 1;
+    }
+    setIndex(checkIndex(randomNumber));
+  }
+  return (
+    <div className="courseMainDiv">
+      <div>
+        <h2>My Courses</h2>
+        <button className="cardRandomBtn" onClick={getRandomCourse}>Random Course Assignment</button>
+      </div>
+      <div className="cardDiv">
+        <button className="prevButton" onClick={prevCourse}>
+          <FaChevronLeft />
+        </button>
+        <div className="card">
+          <div className="cardTitlePrice">
+            <h2 className="cardTitle">{title}</h2>
+            <h4 className="cardPrice">{price}$</h4>
+          </div>
+          <p>{content}</p>
         </div>
-        <div className="cardDiv">
-            {
-                courses.map((course) =>{
-                    return(
-                        <Course key ={course.id} {...course} removeOneCourse = {removeCourse}/>
-                    )
-                })
-            }
-        </div>
+        <button className="prevButton" onClick={nextCourse}>
+          <FaChevronRight />
+        </button>
+        {
+          // courses.map((course) =>{
+          //     return(
+          //         <Course key ={course.id} {...course} removeOneCourse = {removeCourse}/>
+          //     )
+          // })
+        }
+      </div>
     </div>
-    );
+  );
 }
 
 export default Courses;
